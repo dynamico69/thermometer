4232dd4c9dbf958680fb2a31794fb361006c6106
dynamico69 <44874427+dynamico69@users.noreply.github.com>
2020-11-21 15:34:56 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,24 @@
-let temp = 0
 input.onButtonPressed(Button.A, function () {
-    temp = input.temperature()
-    basic.showNumber(temp)
+    vogel.change(LedSpriteProperty.Y, -1)
+    music.playTone(440, music.beat(BeatFraction.Sixteenth))
+})
+input.onButtonPressed(Button.B, function () {
+    vogel.change(LedSpriteProperty.Y, 1)
+    music.playTone(440, music.beat(BeatFraction.Sixteenth))
+})
+let vogel: game.LedSprite = null
+vogel = game.createSprite(0, 2)
+vogel.set(LedSpriteProperty.Blink, 300)
+basic.forever(function () {
+    let hindernisse: game.LedSprite[] = []
+    for (let hindernis of hindernisse) {
+        hindernis.change(LedSpriteProperty.X, -1)
+    }
+    basic.pause(1000)
+})
+basic.forever(function () {
+    let list: number[] = []
+    while (list.length == 0) {
+    	
+    }
 })
