10b6612beb7962f9910054d8805c9f350e168419
dynamico69 <44874427+dynamico69@users.noreply.github.com>
2020-11-21 17:01:09 +0100

    Crashy Bird

--- main.ts
+++ main.ts
@@ -1,24 +1,39 @@
 input.onButtonPressed(Button.A, function () {
-    vogel.change(LedSpriteProperty.Y, -1)
+    bird.change(LedSpriteProperty.Y, -1)
     music.playTone(440, music.beat(BeatFraction.Sixteenth))
 })
 input.onButtonPressed(Button.B, function () {
-    vogel.change(LedSpriteProperty.Y, 1)
+    bird.change(LedSpriteProperty.Y, 1)
     music.playTone(440, music.beat(BeatFraction.Sixteenth))
 })
-let vogel: game.LedSprite = null
-vogel = game.createSprite(0, 2)
-vogel.set(LedSpriteProperty.Blink, 300)
+let emtyObstacleY = 0
+let ticks = 0
+let bird: game.LedSprite = null
+let Index = 0
+let obstacles: game.LedSprite[] = []
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
     let hindernisse: game.LedSprite[] = []
+    while (hindernisse.length > 0 && hindernisse[0] == 0) {
+        hindernisse.removeAt(0).delete()
+    }
     for (let hindernis of hindernisse) {
         hindernis.change(LedSpriteProperty.X, -1)
     }
-    basic.pause(1000)
-})
-basic.forever(function () {
-    let list: number[] = []
-    while (list.length == 0) {
-    	
+    if (ticks % 3 == 0) {
+        emtyObstacleY = randint(0, 4)
+        for (let Index = 0; Index <= 4; Index++) {
+            if (Index != emtyObstacleY) {
+                obstacles.push(game.createSprite(4, Index))
+            }
+        }
+        for (let obstacle of obstacles) {
+            if (obstacle.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X)) {
+                game.gameOver()
+            }
+        }
+        ticks += 1
+        basic.pause(1000)
     }
 })
