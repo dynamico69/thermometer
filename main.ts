b82e18fa792a3d69cc2c7fff7dc8423bcf94d66c
dynamico69 <44874427+dynamico69@users.noreply.github.com>
2020-11-21 20:54:46 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-    bird.change(LedSpriteProperty.Y, -1)
-    music.playTone(440, music.beat(BeatFraction.Sixteenth))
+    Vogel.change(LedSpriteProperty.Y, -1)
+    music.playTone(330, music.beat(BeatFraction.Sixteenth))
 })
 input.onButtonPressed(Button.B, function () {
-    bird.change(LedSpriteProperty.Y, 1)
+    Vogel.change(LedSpriteProperty.Y, 1)
     music.playTone(440, music.beat(BeatFraction.Sixteenth))
 })
-let emtyObstacleY = 0
+let emptyObstacleY = 0
 let ticks = 0
-let bird: game.LedSprite = null
-let Index = 0
-let obstacles: game.LedSprite[] = []
-bird = game.createSprite(0, 2)
-bird.set(LedSpriteProperty.Blink, 300)
+let Vogel: game.LedSprite = null
+let speed = 750
+let index = 0
+let hindernisse: game.LedSprite[] = []
+Vogel = game.createSprite(0, 2)
+Vogel.set(LedSpriteProperty.Blink, 300)
 basic.forever(function () {
-    let hindernisse: game.LedSprite[] = []
-    while (hindernisse.length > 0 && hindernisse[0] == 0) {
+    while (hindernisse.length > 0 && hindernisse[0].get(LedSpriteProperty.X) == 0) {
         hindernisse.removeAt(0).delete()
     }
-    for (let hindernis of hindernisse) {
-        hindernis.change(LedSpriteProperty.X, -1)
+    for (let hindernis2 of hindernisse) {
+        hindernis2.change(LedSpriteProperty.X, -1)
     }
     if (ticks % 3 == 0) {
-        emtyObstacleY = randint(0, 4)
-        for (let Index = 0; Index <= 4; Index++) {
-            if (Index != emtyObstacleY) {
-                obstacles.push(game.createSprite(4, Index))
+        emptyObstacleY = randint(0, 4)
+        for (let index2 = 0; index2 <= 4; index2++) {
+            if (index2 != emptyObstacleY) {
+                hindernisse.push(game.createSprite(4, index2))
             }
         }
-        for (let obstacle of obstacles) {
-            if (obstacle.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X)) {
-                game.gameOver()
-            }
+    }
+    for (let hindernis3 of hindernisse) {
+        if (hindernis3.get(LedSpriteProperty.X) == Vogel.get(LedSpriteProperty.X) && hindernis3.get(LedSpriteProperty.Y) == Vogel.get(LedSpriteProperty.Y)) {
+            music.playMelody("E B C5 A B G A F ", 240)
+            game.gameOver()
         }
-        ticks += 1
-        basic.pause(1000)
+    }
+    ticks += 1
+    basic.pause(speed)
+    if (speed > 400) {
+        speed = speed - 10
     }
 })
